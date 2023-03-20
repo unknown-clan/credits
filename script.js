e93ec50af91230ade1a33a549c318b8dc46f9aa5
la-ser <77540680+la-ser@users.noreply.github.com>
2023-03-20 18:25:16 +0100

    1

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-/*document.getElementById('showCredis').addEventListener('click', () =>{*/
-const cName = document.getElementById('cName')
-const cDesc = document.getElementById('cDesc')
+const cName = document.getElementById("cName");
+const cDesc = document.getElementById("cDesc");
 
-function showCredits(creator) {
-if (creator == arda){
-cName.innerHTML = "Unknown_Arda"
-cDesc.innerHTML = "???"
-} else if (creator == laser){
-cName.innerHTMl = "laser"
-cDesc.innerHTML = "Programmer based in Germany."
-} else {
-cName.innerHTML = "Unknown Games"
-cDesc.innerHTML = "made by arda & laser"
-}
+document.getElementById("showCredisLaser").addEventListener("click", () => {
+  console.log("laser");
+  cName.innerHTML = "laser";
+  cDesc.innerHTML = "Programer based in Germany.";
+});
+
+document.getElementById("showCredisArda").addEventListener("click", () => {
+  console.log("arda");
+  cName.innerHTML = "Unknown_Arda";
+  cDesc.innerHTML = "???";
+});
